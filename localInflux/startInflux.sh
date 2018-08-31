05435e434feb50d6fe3d40446ea9ec70dee4c359
Arch-vile <mikko.ravimo@gmail.com>
2018-08-31 21:10:34 +0300

    set influx data path

--- localInflux/startInflux.sh
+++ localInflux/startInflux.sh
@@ -1,1 +1,7 @@
-docker run --name=localInfluxDB --rm -p 8086:8086 -v $PWD:/var/lib/influxdb influxdb
+
+if [ -z "$INFLUX_DATA_PATH" ]; then
+	echo "INFLUX_DATA_PATH not set!!!!!!"
+	exit 1;
+fi
+
+docker run --name=localInfluxDB --rm -p 8086:8086 -v $INFLUX_DATA_PATH:/var/lib/influxdb influxdb
